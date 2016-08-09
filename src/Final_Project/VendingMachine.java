6e6452e6e1d90cea22f86b2ad535712ea8bc6159
ron.pearl <ron.pearl@rpearl-31349.canyon.com>
2016-08-09 14:36:04 -0700

    Initial Commit

---
+++ src/Final_Project/VendingMachine.java
@@ -0,0 +1,20 @@
+/**
+ * 	Program:	Vending Machine
+ * 	Author:		Ronald Pearl
+ * 	Summary:	Final Project for CST-115 class
+ * 	Date:		8/8/16 - 9/25/16
+ * 	Description:
+ * 		You will create an application that models a vending machine. The vending machine 
+ * 		will have two types of users – a customer, who will be able to select a product, 
+ * 		and a BOSS, who will be able to maintain the collection of products in the machine.  
+ * 		The machine itself will have a graphical interface that displays the available 
+ * 		products and allows the user to make selections. The BOSS will have a separate 
+ * 		interface that provides a variety of maintenance options.  The machine will 
+ * 		provide a means of alternating between the two types of users.
+ */
+
+package Final_Project;
+
+public class VendingMachine {
+	
+}
