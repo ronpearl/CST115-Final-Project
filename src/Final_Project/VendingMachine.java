ed0ee66f0418bbec869adcfe875a33aa5fb4f9d0
ron.pearl <ron.pearl@rpearl-31349.canyon.com>
2016-08-31 14:03:07 -0700

    Updated quantity after purchase

--- src/Final_Project/VendingMachine.java
+++ src/Final_Project/VendingMachine.java
@@ -22,6 +22,8 @@ import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos; 
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -41,6 +43,7 @@ public class VendingMachine extends Application {
             + "-fx-border-width: 1;\n"
             + "-fx-border-style: solid;\n";
 	Font fontBold = Font.font("Sans-Serif",FontWeight.BOLD, FontPosture.REGULAR, 16);
+	ArrayList<Product> productsInSelectedCategory = new ArrayList();
 	
 	@Override
 	public void start(Stage primaryStage) {		
@@ -50,7 +53,7 @@ public class VendingMachine extends Application {
 		// 	Chips:	2
 		// 	Drink:	3
 		// 	Gum:	4
-		mainDisp.addProduct(new Chips("Doritos", 1.99, 1, "A2", 2234, "Doritos are wonderful triangle chips", 135, false));
+		mainDisp.addProduct(new Chips("Doritos", 1.99, 0, "A2", 2234, "Doritos are wonderful triangle chips", 135, false));
 		mainDisp.addProduct(new Chips("Cheetos", 2.49, 5, "B3", 2155, "Cheetos are cheesy crunch balls", 155, false));
 		mainDisp.addProduct(new Candy("Reeses", 2.09, 3, "C1", 1435, "Chocolate and peanut butter matched together", 240, 1));
 		mainDisp.addProduct(new Gum("Trident", 0.89, 5, "D4", 4999, "Classic trident gum", 5, "Medium", true));
@@ -175,8 +178,11 @@ public class VendingMachine extends Application {
 			}
 		}
 		
+		// Save products to accessible variable
+		productsInSelectedCategory = prodsInThisCat;
+		
 		// Now we need to build the pane and list out the items in this category
-		buildCategoryItems(prodsInThisCat);
+		buildCategoryItems();
 	}
 	
 	
@@ -185,7 +191,10 @@ public class VendingMachine extends Application {
 	 * 
 	 * @param prodsInThisCat
 	 */
-	public void buildCategoryItems(ArrayList prodsInThisCat) {
+	public void buildCategoryItems() {
+		// Clear the grid
+		mainCategoryPane.getChildren().clear();
+		
 		mainCategoryPane.setAlignment(Pos.CENTER);
 		mainCategoryPane.setPadding(new Insets(10, 10, 10, 10));
 		mainCategoryPane.setHgap(8);
@@ -197,15 +206,15 @@ public class VendingMachine extends Application {
 		});
 				
 		// Calculate where the back button should be placed based upon number of rows required
-		mainCategoryPane.add(backBtn, 0, (int) ( Math.ceil( (prodsInThisCat.size() / 2) + 1) ), 2, 1);
+		mainCategoryPane.add(backBtn, 0, (int) ( Math.ceil( (productsInSelectedCategory.size() / 2) + 1) ), 2, 1);
 		mainCategoryPane.setHalignment(backBtn, HPos.CENTER);
 		
 		// Setup rows and columns to define the layout
 		int row = 0;
 		int col = 0;
 		
-		for (int i = 0; i < prodsInThisCat.size(); i++) {
-			mainCategoryPane.add(new buildCategoryItems((Product) prodsInThisCat.get(i)), col, row);
+		for (int i = 0; i < productsInSelectedCategory.size(); i++) {
+			mainCategoryPane.add(new buildCategoryItems((Product) productsInSelectedCategory.get(i)), col, row);
 			
 			col++;
 			
@@ -236,10 +245,35 @@ public class VendingMachine extends Application {
 			Label name = new Label(prod.getName());
 			Label price = new Label(String.valueOf(prod.getPrice()));
 			Label desc = new Label(prod.getDescription());
+			Label qty = new Label("Qty Available: " + String.valueOf(prod.getQuantity()));
 			Button buyBtn = new Button("Buy");
+			
 			buyBtn.setOnAction(value ->  {
 				// Purchase the item
 				// Currently we are just going to display an alert with the items details
+				
+				// Check qty available first
+				if (prod.getQuantity() <= 0) {
+					Alert alert = new Alert(AlertType.ERROR);
+					alert.setTitle("Item Unavailable");
+					alert.setHeaderText("Item Unavailable");
+				    alert.setContentText("Sorry, there are no " + prod.getName() + " items left. Please try another item.");
+				    alert.showAndWait();
+				} else {
+					// Update qty of current product
+					prod.setQuantity(prod.getQuantity() - 1);
+					
+					// Show purchase alert
+					Alert alert = new Alert(AlertType.INFORMATION);
+					alert.setTitle("Purchase Details");
+					alert.setHeaderText("Here are the details of your vending purchase");
+				    alert.setContentText("Item Purchased: \t" + prod.getName() +
+				    		"\nPrice: \t\t" + prod.getPrice());
+				    alert.showAndWait();
+				    
+				    // Re-do category items panel
+				    buildCategoryItems();
+				}
 				
 			});
 			
@@ -250,7 +284,8 @@ public class VendingMachine extends Application {
 			pane2.add(name, 0, 0);
 			pane2.add(price, 1, 0);
 			pane2.add(desc, 0, 1, 2, 1);
-			pane2.add(buyBtn, 0, 2, 2, 1);
+			pane2.add(qty, 0, 2, 2, 1);
+			pane2.add(buyBtn, 0, 3, 2, 1);
 			
 			pane2.setHalignment(price, HPos.RIGHT);
 			pane2.setHalignment(buyBtn, HPos.CENTER);
