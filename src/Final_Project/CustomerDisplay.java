cbce9a6c38d40d333fb1dcc5e52e4931d4acfad2
ron.pearl <ron.pearl@rpearl-31349.canyon.com>
2016-09-21 07:32:01 -0700

    Added Customer Que purchasing classes and the required CSV file

---
+++ src/Final_Project/CustomerDisplay.java
@@ -0,0 +1,11 @@
+/**
+ * Class:		CustomerDisplay
+ * Date:		9/21/16
+ * Use:			Uses the Customer Que class and displays the results onto the screen.
+ */
+
+package Final_Project;
+
+public class CustomerDisplay extends ProcessCustomerQueue {
+	
+}
---
+++ src/Final_Project/ProcessCustomerQueue.java
@@ -0,0 +1,15 @@
+/**
+ * Class:		ProcessCustomerQueue
+ * Date:		9/21/16
+ * Use:			This class will utilize a CSV file containing peoples names and a choice
+ * 				they will make in the machine. Then it will cue up those purchases
+ * 				and display it onto the screen.
+ */
+
+package Final_Project;
+
+public class ProcessCustomerQueue extends Product {
+	
+	
+	
+}
